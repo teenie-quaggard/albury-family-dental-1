68872f74d1025b1f3709a0ee65f349a7f401f452
Oliver Smith-Wellnitz <oliversw.artist@gmail.com>
2019-07-21 17:17:11 +0100

    Add gatsby-source-filesystem and gatsby-transformer-remark plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,5 +31,20 @@ module.exports = {
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
     `gatsby-plugin-netlify-cms`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/static/assets`,
+        name: "images",
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `content`,
+        path: `${__dirname}/content`,
+      },
+    },
+    "gatsby-transformer-remark",
   ],
 }
